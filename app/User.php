e77e66879408dfe15f75175ca903ac52e874bd7d
davidfd54 <david.fariasd96@gmail.com>
2019-05-13 14:36:47 -0400

    Update User.php

--- app/User.php
+++ app/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
         'name', 'email', 'password', 'tipoUsuario',
     ];
 
+    protected $primaryKey = 'idUsuario';
+
     /**
      * The attributes that should be hidden for arrays.
      *
